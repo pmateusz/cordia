2f79b1189d1452f2761fc61a7ee997b74bdd7d99
pmateusz <pmateusz@users.noreply.github.com>
2017-11-08 16:07:54 +0000

    Create list_employees_in_aom.sql

---
+++ sql/people/list_employees_in_aom.sql
@@ -0,0 +1,22 @@
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+
+DECLARE @AOM_CODE INT;
+SET @AOM_CODE = 12;
+
+SELECT employee_position.EmployeePositionID as 'employee_position_id'
+  FROM People.dbo.EmployeePosition AS employee_position
+       INNER JOIN People.dbo.Position AS position
+       ON employee_position.PositionID = position.PositionID
+       INNER JOIN People.dbo.CarePositions AS care_position
+       ON position.PositionName = care_position.PositionName
+       LEFT OUTER JOIN SparkCare.dbo.AomOpStruct AS aom_struct
+       ON position.OpStructID = aom_struct.OpStructID
+       LEFT OUTER JOIN SparkCare.dbo.AomOrgUnit as aom_unit
+       ON position.OrgUnitID = aom_unit.OrgUnitId
+       INNER JOIN SparkCare.dbo.AomBase AS aom_struct_base
+       ON aom_struct_base.AomID = aom_struct.AomId
+       INNER JOIN SparkCare.dbo.AomBase AS aom_unit_base
+       ON aom_unit_base.AomID = aom_unit.AomID
+ WHERE (employee_position.EndDate IS NULL OR employee_position.EndDate > GETDATE())
+       AND (aom_struct_base.AomBaseID = @AOM_CODE OR aom_unit_base.AomBaseID = @AOM_CODE)
+ ORDER BY employee_position_id
