eca68298d4657ac04c669a5c7e8b33cfaab8918c
pmateusz <pmateusz@users.noreply.github.com>
2017-11-10 14:04:50 +0000

    Update list_visits_within_time_windows.sql

--- sql/spark_care/list_visits_within_time_windows.sql
+++ sql/spark_care/list_visits_within_time_windows.sql
@@ -1,15 +1,34 @@
-USE SparkCare;
+-- Lists all visits requested within the specified time interval and within the specified area
+
 SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
 GO
 
-DECLARE @begin DATE, @end DATE;
-SET @begin = '2017-10-25';
-SET @end = '2017-10-31';
+DECLARE @VISITS_FROM DATETIME, @VISITS_TO DATETIME;
+SET @VISITS_FROM = '2017-10-31';
+SET @VISITS_TO = '2017-11-30';
 
-SELECT *
-  FROM dbo.Visit AS visit
-       INNER JOIN dbo.VisitType AS visit_type
+DECLARE @AOM_CODE INT;
+SET @AOM_CODE = 617;
+
+SELECT visit.VisitID AS 'visit_id',
+	   visit.ServiceUserID AS 'service_user_id',
+	   visit.VisitDate AS 'visit_date',
+	   visit.RequestedVisitTime AS 'requested_visit_time',
+	   visit.RequestedVisitDuration AS 'requested_visit_duration',
+	   service_user_view.AomID AS 'aom_id',
+	   service_user_view.Street AS 'street',
+	   service_user_view.Town AS 'town',
+	   service_user_view.Postcode AS 'post_code'
+  FROM SparkCare.dbo.Visit AS visit
+	   INNER JOIN SparkCare.dbo.ServiceUserView AS service_user_view
+	   ON visit.ServiceUserID = service_user_view.ServiceUserID
+	   INNER JOIN SparkCare.dbo.AomView AS aom_view
+	   ON service_user_view.AomID = aom_view.AomID
+	   LEFT OUTER JOIN SparkCare.dbo.ServiceType AS service_type
+       ON visit.ServiceTypeID = service_type.ServiceTypeID
+       LEFT OUTER JOIN SparkCare.dbo.VisitType AS visit_type
        ON visit.VisitTypeID = visit_type.VisitTypeID
-       LEFT OUTER JOIN dbo.VisitAssignment AS visit_assignment
-       ON visit_assignment.VisitID = visit.VisitID
- WHERE visit.VisitDate >= @begin AND visit.VisitDate <= @end
+ WHERE visit.VisitDate >= @VISITS_FROM AND visit.VisitDate < @VISITS_TO
+		AND aom_view.AomID = @AOM_CODE
+		AND visit.Suspended = 0
+    
