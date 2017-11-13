2c1ab7ea9f36fbaa694d0d4f085ac6f3d603ef50
pmateusz <pmateusz@users.noreply.github.com>
2017-11-13 14:35:45 +0000

    Update list_aom.sql

--- sql/people/list_aom.sql
+++ sql/people/list_aom.sql
@@ -5,11 +5,10 @@ GO
 
 SELECT aom_details.AomCode AS 'aom_code',
        aom_base.AomId AS 'aom_id',
-	   aom_details.AreaCode as 'area_code',
-	   aom_details.AreaNo as 'area_no',
-	   aom_details.Name as 'primary_manager'
+       aom_details.AreaCode as 'area_code',
+       aom_details.AreaNo as 'area_no'
   FROM People.dbo.AomDetails AS aom_details
-	   INNER JOIN SparkCare.dbo.AomBase as aom_base
-	   ON aom_base.AomBaseID = aom_details.AomCode
+       INNER JOIN SparkCare.dbo.AomBase as aom_base
+       ON aom_base.AomBaseID = aom_details.AomCode
  WHERE aom_details.AreaCode IS NOT NULL
  ORDER BY aom_code, area_no
