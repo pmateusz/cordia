d527cf74b015190044bdcc86fe3b32e3f5313b6a
pmateusz <pmateusz@users.noreply.github.com>
2017-11-14 16:52:57 +0000

    Delete OBSOLETE_question_aom_assigned_shifts.sql

--- sql/people/OBSOLETE_question_aom_assigned_shifts.sql
+++
@@ -1,9 +0,0 @@
---- Seems that the table dbo.AomAssignedShifts is not used is that correct?
-
-USE People;
-SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
-GO
-
-SELECT *
-  FROM dbo.AomAssignedShifts AS assigned_shifts
- WHERE assigned_shifts.NoOfPositions > 0
