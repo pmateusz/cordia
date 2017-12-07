b87ff5ce662e048771751d92aca96a7b7b72e68a
Mateusz Polnik <mateusz.polnik@strath.ac.uk>
2017-12-07 10:58:11 +0000

    Respect contractual breaks. Print solution.

--- src/main/rows-solver.cpp
+++ src/main/rows-solver.cpp
@@ -95,6 +95,36 @@ public:
         return carer_pair.first;
     }
 
+    std::vector<operations_research::IntervalVar *> Breaks(operations_research::Solver *const solver,
+                                                           const operations_research::RoutingModel::NodeIndex carer) const {
+        std::vector<operations_research::IntervalVar *> result;
+
+        const auto &diary = Diary(carer);
+
+        boost::posix_time::ptime last_end_time(diary.date());
+        boost::posix_time::ptime next_day(diary.date() + boost::gregorian::date_duration(1));
+
+        BreakType break_type = BreakType::BEFORE_WORKDAY;
+        for (const auto &event : diary.events()) {
+
+            result.push_back(CreateBreak(solver,
+                                         last_end_time.time_of_day(),
+                                         event.begin() - last_end_time,
+                                         GetBreakLabel(carer, break_type)));
+
+            last_end_time = event.end();
+            break_type = BreakType::BREAK;
+        }
+
+        break_type = BreakType::AFTER_WORKDAY;
+        result.push_back(CreateBreak(solver,
+                                     last_end_time.time_of_day(),
+                                     next_day - last_end_time,
+                                     GetBreakLabel(carer, break_type)));
+
+        return std::move(result);
+    }
+
     std::vector<operations_research::RoutingModel::NodeIndex> Carers() const {
         std::vector<operations_research::RoutingModel::NodeIndex> result(problem_.carers().size());
         std::iota(std::begin(result), std::end(result), operations_research::RoutingModel::NodeIndex(0));
@@ -109,7 +139,117 @@ public:
         return static_cast<int>(problem_.carers().size());
     }
 
+    void DisplayPlan(const operations_research::RoutingModel &routing,
+                     const operations_research::Assignment &plan,
+                     bool use_same_vehicle_costs,
+                     int64 max_nodes_per_group,
+                     int64 same_vehicle_cost,
+                     const operations_research::RoutingDimension &time_dimension) {
+        std::stringstream out;
+        out << boost::format("Cost %1% ") % plan.ObjectiveValue() << std::endl;
+
+        std::stringstream dropped_stream;
+        for (int order = 1; order < routing.nodes(); ++order) {
+            if (plan.Value(routing.NextVar(order)) == order) {
+                if (dropped_stream.rdbuf()->in_avail() == 0) {
+                    dropped_stream << ' ' << order;
+                } else {
+                    dropped_stream << ',' << ' ' << order;
+                }
+            }
+        }
+
+        if (dropped_stream.rdbuf()->in_avail() > 0) {
+            out << "Dropped orders:" << dropped_stream.str() << std::endl;
+        }
+
+        if (use_same_vehicle_costs) {
+            int group_size = 0;
+            int64 group_same_vehicle_cost = 0;
+            std::set<int64> visited;
+            const operations_research::RoutingModel::NodeIndex kFirstNodeAfterDepot(1);
+            for (operations_research::RoutingModel::NodeIndex order = kFirstNodeAfterDepot;
+                 order < routing.nodes(); ++order) {
+                ++group_size;
+                visited.insert(plan.Value(routing.VehicleVar(routing.NodeToIndex(order))));
+                if (group_size == max_nodes_per_group) {
+                    if (visited.size() > 1) {
+                        group_same_vehicle_cost += (visited.size() - 1) * same_vehicle_cost;
+                    }
+                    group_size = 0;
+                    visited.clear();
+                }
+            }
+            if (visited.size() > 1) {
+                group_same_vehicle_cost += (visited.size() - 1) * same_vehicle_cost;
+            }
+            LOG(INFO) << "Same vehicle costs: " << group_same_vehicle_cost;
+        }
+
+        for (int route_number = 0; route_number < routing.vehicles(); ++route_number) {
+            int64 order = routing.Start(route_number);
+            out << boost::format("Route %1%: ") % route_number;
+
+            if (routing.IsEnd(plan.Value(routing.NextVar(order)))) {
+                out << "Empty" << std::endl;
+            } else {
+                while (true) {
+                    operations_research::IntVar *const time_var =
+                            time_dimension.CumulVar(order);
+                    operations_research::IntVar *const slack_var =
+                            routing.IsEnd(order) ? nullptr : time_dimension.SlackVar(order);
+                    if (slack_var != nullptr && plan.Contains(slack_var)) {
+                        out << boost::format("%1% Time(%2%, %3%) Slack(%4%, %5%) -> ")
+                               % order
+                               % plan.Min(time_var) % plan.Max(time_var)
+                               % plan.Min(slack_var) % plan.Max(slack_var);
+                    } else {
+                        out << boost::format("%1% Time(%2%, %3%) ->")
+                               % order
+                               % plan.Min(time_var)
+                               % plan.Max(time_var);
+                    }
+                    if (routing.IsEnd(order)) break;
+                    order = plan.Value(routing.NextVar(order));
+                }
+                out << std::endl;
+            }
+        }
+        LOG(INFO) << out.str();
+    }
+
 private:
+    enum class BreakType {
+        BREAK, BEFORE_WORKDAY, AFTER_WORKDAY
+    };
+
+    static operations_research::IntervalVar *CreateBreak(operations_research::Solver *const solver,
+                                                         boost::posix_time::time_duration start_time,
+                                                         boost::posix_time::time_duration duration,
+                                                         std::string label) {
+        static const auto IS_OPTIONAL = false;
+
+        return solver->MakeFixedDurationIntervalVar(
+                /*start min*/ start_time.total_seconds(),
+                /*start max*/ start_time.total_seconds(),
+                              duration.total_seconds(),
+                              IS_OPTIONAL,
+                              label);
+    }
+
+    static std::string GetBreakLabel(const operations_research::RoutingModel::NodeIndex carer, BreakType break_type) {
+        switch (break_type) {
+            case BreakType::BEFORE_WORKDAY:
+                return (boost::format("Carer '%1%' before workday") % carer).str();
+            case BreakType::AFTER_WORKDAY:
+                return (boost::format("Carer '%1%' after workday") % carer).str();
+            case BreakType::BREAK:
+                return (boost::format("Carer '%1%' break") % carer).str();
+            default:
+                throw std::domain_error((boost::format("Handling label '%1%' is not implemented") % carer).str());
+        }
+    }
+
     const rows::Problem &problem_;
     rows::LocationContainer location_container_;
 };
@@ -185,7 +325,11 @@ int main(int argc, char **argv) {
         }
     }
 
-    rows::Problem reduced_problem(visits_to_use, carers_to_use);
+    // FIXME
+    std::vector<rows::Visit> test_visits;
+    std::copy(std::cbegin(visits_to_use), std::cbegin(visits_to_use) + 10, std::back_inserter(test_visits));
+
+    rows::Problem reduced_problem(test_visits, carers_to_use);
     DCHECK(reduced_problem.IsAdmissible());
 
     SolverWrapper wrapper(reduced_problem);
@@ -201,27 +345,7 @@ int main(int argc, char **argv) {
 
     operations_research::Solver *const solver = routing.solver();
     for (const auto &carer_index : wrapper.Carers()) {
-        const auto &diary = wrapper.Diary(carer_index);
-        const auto &begin_time = diary.begin_time();
-        const auto &end_time = diary.end_time();
-
-        std::vector<operations_research::IntervalVar *> breaks{
-                solver->MakeFixedDurationIntervalVar(
-                        /*start min*/ 0,
-                        /*start max*/ 0,
-                        /*break duration*/ begin_time.total_seconds(),
-                        /*optional*/ false,
-                                      (boost::format("Carer '%1%; before working hours") % carer_index.value()).str()),
-                // TODO: breaks within day are ignored
-                solver->MakeFixedDurationIntervalVar(
-                        /*start min*/ end_time.total_seconds(),
-                        /*start max*/ end_time.total_seconds(),
-                        /*break duration*/ kHorizon - end_time.total_seconds(),
-                        /*optional*/ false,
-                                      (boost::format("Carer '%1%; after working hours") % carer_index.value()).str())
-        };
-
-        time_dimension->SetBreakIntervalsOfVehicle(std::move(breaks), carer_index.value());
+        time_dimension->SetBreakIntervalsOfVehicle(wrapper.Breaks(solver, carer_index), carer_index.value());
     }
 
     // set time windows
@@ -261,6 +385,12 @@ int main(int argc, char **argv) {
         LOG(INFO) << "No solution found.";
         return STATUS_ERROR;
     }
+    wrapper.DisplayPlan(routing,
+                        *solution,
+            /*use_same_vehicle_costs=*/false,
+            /*max_nodes_per_group=*/0,
+            /*same_vehicle_cost=*/0,
+                        routing.GetDimensionOrDie(kTime));
 
     return STATUS_OK;
 }
