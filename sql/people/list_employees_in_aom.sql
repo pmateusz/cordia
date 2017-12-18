7cc0a1dcc8ee459bb9cae5178d53dcde8138a8ec
pmateusz <pmateusz@users.noreply.github.com>
2017-12-18 15:08:36 +0000

    Include feedback from code review

--- sql/people/list_employees_in_aom.sql
+++ sql/people/list_employees_in_aom.sql
@@ -7,34 +7,21 @@ DECLARE @START_TIME DATETIME, @END_TIME DATETIME;
 SET @START_TIME = '2017-10-31';
 SET @END_TIME = '2017-11-30';
 
-DECLARE @AOM_CODE INT;
-SET @AOM_CODE = 1;
+DECLARE @AOM_ID INT;
+SET @AOM_ID = 627;
 
-SELECT employee_position.EmployeePositionID AS 'employee_position_id',
+SELECT carer_view.CarerId AS 'carer_id',
 	CASE
-		WHEN employee_position.StartDate < @START_TIME THEN @START_TIME
-		ELSE employee_position.StartDate
+		WHEN carer_view.HiredDate < @START_TIME THEN @START_TIME
+		ELSE carer_view.HiredDate
 	END AS 'start_time',
 	CASE
-		WHEN employee_position.EndDate IS NOT NULL AND employee_position.EndDate < @END_TIME THEN employee_position.EndDate
-		ELSE COALESCE(employee_position.EndDate, @END_TIME)
+		WHEN carer_view.TerminatedDate IS NOT NULL AND carer_view.TerminatedDate < @END_TIME THEN carer_view.TerminatedDate
+		ELSE COALESCE(carer_view.TerminatedDate, @END_TIME)
 	END AS 'end_time',
-	aom_struct_base.AomBaseID AS 'struct_aom',
-	aom_unit_base.AomBaseID AS 'unit_aom'
-  FROM People.dbo.EmployeePosition AS employee_position
-       INNER JOIN People.dbo.Position AS position
-       ON employee_position.PositionID = position.PositionID
-       INNER JOIN People.dbo.CarePositions AS care_position
-       ON position.PositionName = care_position.PositionName
-       INNER JOIN SparkCare.dbo.AomOpStruct AS aom_struct
-       ON position.OpStructID = aom_struct.OpStructID
-       INNER JOIN SparkCare.dbo.AomOrgUnit as aom_unit
-       ON position.OrgUnitID = aom_unit.OrgUnitId
-       INNER JOIN SparkCare.dbo.AomBase AS aom_struct_base
-       ON aom_struct_base.AomID = aom_struct.AomId
-       INNER JOIN SparkCare.dbo.AomBase AS aom_unit_base
-       ON aom_unit_base.AomID = aom_unit.AomID
- WHERE employee_position.StartDate < @END_TIME
-       AND (employee_position.EndDate IS NULL or employee_position.EndDate > @START_TIME)
-	     AND (aom_unit_base.AomBaseID = @AOM_CODE OR aom_struct_base.AomBaseID = @AOM_CODE)
- ORDER BY employee_position_id, start_time
+	carer_view.AomId AS 'aom_id'
+  FROM SparkCare.dbo.CarerView AS carer_view
+ WHERE carer_view.HiredDate < @END_TIME
+       AND (carer_view.TerminatedDate IS NULL or carer_view.TerminatedDate > @START_TIME)
+	   AND (carer_view.AomId = @AOM_ID)
+ ORDER BY carer_id, start_time
