7fd347dd0933eb5b1337f61a6889d6688e799bf4
pmateusz <pmateusz@users.noreply.github.com>
2017-12-18 14:52:20 +0000

    Update query after feedback from code review

--- sql/spark_care/list_visits_within_time_windows.sql
+++ sql/spark_care/list_visits_within_time_windows.sql
@@ -10,26 +10,24 @@ SET @END_TIME = '2017-11-30';
 DECLARE @AOM_CODE INT;
 SET @AOM_CODE = 1;
 
-SELECT visit.VisitID AS 'visit_id',
+SELECT TOP(100) visit.VisitID AS 'visit_id',
        visit.ServiceUserID AS 'service_user_id',
-       visit.VisitDate AS 'visit_date',
+       CAST(visit.VisitDate AS DATE) AS 'visit_date',
        visit.RequestedVisitTime AS 'requested_visit_time',
        visit.RequestedVisitDuration AS 'requested_visit_duration',
        service_user_view.Street AS 'street',
        service_user_view.Town AS 'town',
        service_user_view.Postcode AS 'post_code',
-       aom_details.AomCode AS 'aom_code'
+       aom.LtAomId AS 'aom_code'
   FROM SparkCare.dbo.Visit AS visit
        INNER JOIN SparkCare.dbo.ServiceUserView AS service_user_view
        ON visit.ServiceUserID = service_user_view.ServiceUserID
-       INNER JOIN SparkCare.dbo.AomView AS aom_view
-       ON service_user_view.AomID = aom_view.AomID
-       INNER JOIN People.dbo.AomDetails AS aom_details
-       ON aom_view.AreaCode = aom_details.AreaCode
+	   LEFT OUTER JOIN Homecare.dbo.ltAom aom
+	   ON aom.LtAomId = service_user_view.AomID
        LEFT OUTER JOIN SparkCare.dbo.ServiceType AS service_type
        ON visit.ServiceTypeID = service_type.ServiceTypeID
        LEFT OUTER JOIN SparkCare.dbo.VisitType AS visit_type
        ON visit.VisitTypeID = visit_type.VisitTypeID
  WHERE visit.VisitDate BETWEEN @START_TIME AND @END_TIME
-       AND aom_details.AomCode = @AOM_CODE
+       AND aom.LtAomId = @AOM_CODE
        AND visit.Suspended = 0
