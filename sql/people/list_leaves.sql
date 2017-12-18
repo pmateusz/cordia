c8827f715d84fe78fe0fb81009b05126128d5877
pmateusz <pmateusz@users.noreply.github.com>
2017-12-18 15:38:23 +0000

    Apply updates suggested in code review

--- sql/people/list_leaves.sql
+++ sql/people/list_leaves.sql
@@ -5,36 +5,25 @@ DECLARE @START_TIME DATETIME, @END_TIME DATETIME;
 SET @START_TIME = '2017-09-1';
 SET @END_TIME = '2017-10-1';
 
-DECLARE @AOM_CODE INT;
-SET @AOM_CODE = 1;
+DECLARE @AOM_ID INT;
+SET @AOM_ID = 637;
 
--- table with EmployeePositionID of carers that are employed within the specified time window who supported the specified aom
-DECLARE @CARERS TABLE(EmployeePositionId INT);
+DECLARE @CARERS TABLE(CarerId INT, EmployeeId INT);
 
 INSERT INTO @CARERS
-SELECT employee_position.EmployeePositionID AS 'employee_position_id'
-  FROM People.dbo.EmployeePosition AS employee_position
-       INNER JOIN People.dbo.Position AS position
-       ON employee_position.PositionID = position.PositionID
-       INNER JOIN People.dbo.CarePositions AS care_position
-       ON position.PositionName = care_position.PositionName
-       INNER JOIN SparkCare.dbo.AomOpStruct AS aom_struct
-       ON position.OpStructID = aom_struct.OpStructID
-       INNER JOIN SparkCare.dbo.AomOrgUnit as aom_unit
-       ON position.OrgUnitID = aom_unit.OrgUnitId
-       INNER JOIN SparkCare.dbo.AomBase AS aom_struct_base
-       ON aom_struct_base.AomID = aom_struct.AomId
-       INNER JOIN SparkCare.dbo.AomBase AS aom_unit_base
-       ON aom_unit_base.AomID = aom_unit.AomID
- WHERE employee_position.StartDate < @END_TIME
-       AND (COALESCE(employee_position.EndDate, @END_TIME) > @START_TIME)
-       AND (aom_unit_base.AomBaseID = @AOM_CODE OR aom_struct_base.AomBaseID = @AOM_CODE)
- ORDER BY employee_position_id;
+SELECT carer_view.CarerId AS 'carer_id', carer_view.EmployeeId AS 'employee_id'
+  FROM SparkCare.dbo.CarerView carer_view
+ WHERE carer_view.HiredDate < @END_TIME
+       AND (COALESCE(carer_view.TerminatedDate, @END_TIME) > @START_TIME)
+       AND (carer_view.AomId = @AOM_ID)
+ ORDER BY carer_id, employee_id;
 
-SELECT leave.EmployeePositionId, leave.StartDate, leave.EndDate, leave.HalfDay, leave.HalfDayPeriod, leave_approval.ApprovalStatus
+SELECT carer.EmployeeId, carer.CarerId, leave.EmployeePositionId, leave.StartDate, leave.EndDate, leave.HalfDay, leave.HalfDayPeriod, leave_approval.ApprovalStatus
   FROM @CARERS AS carer
+	   INNER JOIN People.dbo.EmployeePosition employee_position
+	   ON carer.EmployeeId = employee_position.EmployeeID
        INNER JOIN People.dbo.Leave leave
-       ON leave.EmployeePositionID = carer.EmployeePositionId
+       ON leave.EmployeePositionID = employee_position.EmployeePositionID
        INNER JOIN People.dbo.LtLeaveApprovalType leave_approval
        ON leave.ApprovalStatus = leave_approval.ApprovalTypeID
        INNER JOIN People.dbo.LeaveType AS leave_type
