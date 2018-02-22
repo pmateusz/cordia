f723e493183f679d662c6423aa83181300002d1c
Mateusz Polnik <mateusz.polnik@strath.ac.uk>
2018-02-22 15:40:50 +0000

    Extract information about carer's schedule

--- rows/sql_data_source.py
+++ rows/sql_data_source.py
@@ -4,8 +4,13 @@ import collections
 
 import pyodbc
 
+import itertools
+
 from rows.model.address import Address
 from rows.model.area import Area
+from rows.model.carer import Carer
+from rows.model.diary import Diary
+from rows.model.event import AbsoluteEvent
 from rows.model.problem import Problem
 from rows.model.service_user import ServiceUser
 
@@ -16,11 +21,16 @@ FROM [StrathClyde].[dbo].[ListAom] aom
 ORDER BY aom.area_code"""
 
     LIST_VISITS_QUERY = """SELECT MIN(visits.visit_id) as visit_id, visits.service_user_id, visits.visit_date,
-visits.requested_visit_time, visits.requested_visit_duration, visits.display_address
+visits.requested_visit_time, visits.requested_visit_duration
 FROM dbo.ListVisitsWithinWindow visits
 WHERE visits.visit_date BETWEEN '{0}' AND '{1}' AND visits.aom_code = {2}
 GROUP BY visits.service_user_id, visits.visit_date, visits.requested_visit_duration,
 visits.display_address, visits.requested_visit_time"""
+
+    LIST_SERVICE_USER_QUERY = """SELECT visits.service_user_id, visits.display_address
+    FROM dbo.ListVisitsWithinWindow visits
+    WHERE visits.visit_date BETWEEN '{0}' AND '{1}' AND visits.aom_code = {2}
+    GROUP BY visits.service_user_id, visits.display_address"""
 
     LIST_MULTIPLE_CARER_VISITS_QUERY = """SELECT visits.VisitID as visit_id, COUNT(visits.VisitID) as carer_count
 FROM dbo.ListCarerVisits visits
@@ -28,20 +38,28 @@ WHERE visits.PlannedStartDateTime BETWEEN '{0}' AND '{1}'
 GROUP BY visits.PlannedStartDateTime, visits.VisitID
 HAVING COUNT(visits.VisitID) > 1"""
 
-    CARER_FREQUENCY_QUERY = """SELECT user_visit.service_user_id as 'user', carer_visit.PlannedCarerID as 'carer', COUNT(user_visit.visit_id) AS visits_count, MIN(total_visits.total_visits) AS total_visits, ROUND(CONVERT(float, COUNT(user_visit.visit_id)) / MIN(total_visits.total_visits), 4) as care_continuity
+    CARER_FREQUENCY_QUERY = """SELECT user_visit.service_user_id as 'user', carer_visit.PlannedCarerID as 'carer',
+COUNT(user_visit.visit_id) AS visits_count,
+MIN(total_visits.total_visits) AS total_visits,
+ROUND(CONVERT(float, COUNT(user_visit.visit_id)) / MIN(total_visits.total_visits), 4) as care_continuity
 FROM dbo.ListCarerVisits carer_visit
 INNER JOIN (SELECT DISTINCT service_user_id, visit_id 
-			FROM dbo.ListVisitsWithinWindow
-			WHERE aom_code = {0}) user_visit 
+FROM dbo.ListVisitsWithinWindow
+WHERE aom_code = {0}) user_visit 
 ON carer_visit.VisitID = user_visit.visit_id
 INNER JOIN (SELECT service_user_id as service_user_id, COUNT(visit_id) as total_visits
-			FROM (SELECT DISTINCT service_user_id, visit_id 
-			FROM dbo.ListVisitsWithinWindow
-			WHERE aom_code = {0}) local_visit
-			GROUP BY service_user_id) total_visits
+FROM (SELECT DISTINCT service_user_id, visit_id 
+FROM dbo.ListVisitsWithinWindow
+WHERE aom_code = {0}) local_visit
+GROUP BY service_user_id) total_visits
 ON total_visits.service_user_id = user_visit.service_user_id
 GROUP BY user_visit.service_user_id, carer_visit.PlannedCarerID, total_visits.service_user_id
 ORDER BY user_visit.service_user_id, care_continuity DESC"""
+
+    CARER_WORKING_HOURS = """SELECT CarerId, StartDateTime, EndDateTime
+FROM dbo.ListCarerIntervals
+WHERE AomId={2} AND StartDateTime BETWEEN '{0}' AND '{1}'
+"""
 
     def __init__(self, location_finder):
         self.__location_finder = location_finder
@@ -61,18 +79,14 @@ ORDER BY user_visit.service_user_id, care_continuity DESC"""
             carer_counts[visit_id] = carer_count
 
         visits_by_service_user = {}
-        location_by_service_user = {}
-        address_by_service_user = {}
-
         for row in self.__get_connection().cursor().execute(
                 SqlDataSource.LIST_VISITS_QUERY.format(begin_date, end_date, area.key)).fetchall():
-            visit_key, service_user, visit_date, visit_time, visit_duration, display_address = row
+            visit_key, service_user, visit_date, visit_time, visit_duration = row
 
             carer_count = 1
             if visit_key in carer_counts:
                 carer_count = carer_counts[visit_key]
 
-            address = Address.parse(display_address)
             local_visit = Problem.LocalVisit(key=visit_key,
                                              date=visit_date,
                                              time=visit_time,
@@ -82,6 +96,34 @@ ORDER BY user_visit.service_user_id, care_continuity DESC"""
                 visits_by_service_user[service_user].append(local_visit)
             else:
                 visits_by_service_user[service_user] = [local_visit]
+
+        return [Problem.LocalVisits(service_user=service_user, visits=visits)
+                for service_user, visits in visits_by_service_user.items()]
+
+    def get_carers(self, area, begin_date, end_date):
+        events_by_carer = collections.defaultdict(list)
+        for row in self.__get_connection().cursor().execute(
+                SqlDataSource.CARER_WORKING_HOURS.format(begin_date, end_date, area.key)).fetchall():
+            carer_id, begin_time, end_time = row
+            events_by_carer[carer_id].append(AbsoluteEvent(begin=begin_time, end=end_time))
+        carer_shifts = []
+        for carer in events_by_carer.keys():
+            diaries = [Diary(date=date, events=list(events), schedule_pattern=None)
+                       for date, events
+                       in itertools.groupby(events_by_carer[carer], key=lambda event: event.begin.date())]
+            carer_shift = Problem.CarerShift(carer=Carer(sap_number=carer), diaries=diaries)
+            carer_shifts.append(carer_shift)
+        return carer_shifts
+
+    def get_service_users(self, area, begin_date, end_date):
+        location_by_service_user = {}
+        address_by_service_user = {}
+        for row in self.__get_connection().cursor().execute(
+                SqlDataSource.LIST_SERVICE_USER_QUERY.format(begin_date, end_date, area.key)).fetchall():
+            service_user, raw_address = row
+
+            if service_user not in location_by_service_user:
+                address = Address.parse(raw_address)
                 location = self.__location_finder.find(address)
                 if location is None:
                     logging.error("Failed to find location of the address '%s'", location)
@@ -94,19 +136,11 @@ ORDER BY user_visit.service_user_id, care_continuity DESC"""
             service_user, carer, _carer_visit_count, _total_visit_count, frequency = row
             preference_by_service_user[service_user][carer] = frequency
 
-        # extract service user preference
-        return [Problem.LocalVisits(service_user=ServiceUser(key=service_user,
-                                                             location=location_by_service_user[service_user],
-                                                             address=address_by_service_user[service_user],
-                                                             carer_preference=preference_by_service_user[service_user]),
-                                    visits=visits)
-                for service_user, visits in visits_by_service_user.items()]
-
-    def get_carers(self, area, begin_date, end_date):
-        return []
-
-    def get_service_users(self, area, begin_date, end_date):
-        return []
+        return [ServiceUser(key=service_user,
+                            location=location,
+                            address=address_by_service_user[service_user],
+                            carer_preference=preference_by_service_user[service_user])
+                for service_user, location in location_by_service_user.items()]
 
     def reload(self):
         self.__get_connection_string()
