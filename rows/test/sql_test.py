f3620e5dedb0fcf60804fc350181e42081094457
Mateusz Polnik <mateusz.polnik@strath.ac.uk>
2018-02-23 13:42:35 +0000

    Calculate continuity of care metrics

--- rows/test/sql_test.py
+++ rows/test/sql_test.py
@@ -3,6 +3,8 @@
 import unittest
 import pyodbc
 import pathlib
+import collections
+import statistics
 
 from rows.model.area import Area
 
@@ -26,12 +28,6 @@ def build_connection_string():
     return ';'.join(['{0}={1}'.format(key, value) for key, value in config.items()])
 
 
-# TODO: list carers available in the specified area
-
-# TODO: list visits within date interval in the specified area
-
-# TODO: list schedule of carers in the specified area
-
 class TestSqlIntegration(unittest.TestCase):
     """Test integration with SQL database"""
 
@@ -44,6 +40,61 @@ class TestSqlIntegration(unittest.TestCase):
                            'ORDER BY aom.area_code')
             areas = [Area(key=row[0], code=row[1]) for row in cursor.fetchall()]
             print(areas)
+        except Exception as ex:
+            print(ex)
+
+    def test_get_continuity_of_care(self):
+        try:
+            connection = pyodbc.connect(build_connection_string())
+            cursor = connection.cursor()
+            frequency_results = collections.defaultdict(list)
+            for row in cursor.execute('''SELECT user_visit.service_user_id as 'user', carer_visit.PlannedCarerID as 'carer',
+COUNT(user_visit.visit_id) AS visits_count, MIN(total_visits.total_visits) AS total_visits,
+ROUND(CONVERT(float, COUNT(user_visit.visit_id)) / MIN(total_visits.total_visits), 4) as care_continuity
+FROM dbo.ListCarerVisits carer_visit
+INNER JOIN (SELECT DISTINCT service_user_id, visit_id 
+FROM dbo.ListVisitsWithinWindow
+WHERE aom_code = 617) user_visit 
+ON carer_visit.VisitID = user_visit.visit_id
+INNER JOIN (SELECT service_user_id as service_user_id, COUNT(visit_id) as total_visits
+FROM (SELECT DISTINCT service_user_id, visit_id 
+FROM dbo.ListVisitsWithinWindow
+WHERE aom_code = 617) local_visit
+GROUP BY service_user_id) total_visits
+ON total_visits.service_user_id = user_visit.service_user_id
+GROUP BY user_visit.service_user_id, carer_visit.PlannedCarerID, total_visits.service_user_id
+ORDER BY user_visit.service_user_id, care_continuity DESC''').fetchall():
+                service_user, carer, visits, total_visits, relative_freq = row
+                frequency_results[service_user].append((visits, relative_freq))
+            # calculate weighted average for every carer
+            average_by_service_user = {}
+            for service_user, frequency_pair in frequency_results.items():
+                visits_count = 0
+                nominator = 0.0
+                for visits, relative_frequency in frequency_pair:
+                    visits_count += visits
+                    nominator += relative_frequency * visits_count
+                if visits_count > 0:
+                    average_by_service_user[service_user] = nominator / visits_count
+            print('mean={0}, median={1}, stddev={2}'.format(
+                round(statistics.mean(average_by_service_user.values()), 4),
+                round(statistics.stdev(average_by_service_user.values()), 4),
+                round(statistics.median(average_by_service_user.values())), 4))
+
+            standard_average_by_service_user = {}
+            for service_user, frequency_pair in frequency_results.items():
+                carers = 0
+                nominator = 0.0
+                for visits, relative_frequency in frequency_pair:
+                    carers += 1
+                    nominator += relative_frequency
+                if visits_count > 0:
+                    standard_average_by_service_user[service_user] = nominator / carers
+            print('mean={0}, median={1}, stddev={2}'.format(
+                round(statistics.mean(standard_average_by_service_user.values()), 4),
+                round(statistics.stdev(standard_average_by_service_user.values()), 4),
+                round(statistics.median(standard_average_by_service_user.values())), 4))
+
         except Exception as ex:
             print(ex)
 
