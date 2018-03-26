c23ec6693524a067bd575e67bf90dc320fce82c7
Mateusz Polnik <mateusz.polnik@strath.ac.uk>
2018-03-26 10:51:51 +0100

    Fix the version command

--- rows/version_command.py
+++ rows/version_command.py
@@ -1,5 +1,7 @@
 """Implements the version command"""
 
+import rows.version
+
 
 class Handler:
     """Implements the version command"""
@@ -11,6 +13,6 @@ class Handler:
         self.__console = application.console
 
     def __call__(self, command):
-        message = '{0} version {1}'.format(self.__application.PROGRAM_NAME, self.__application.VERSION)
+        message = '{0} version {1}'.format(self.__application.PROGRAM_NAME, rows.version.VERSION)
         self.__console.write_line(message)
         return 0
