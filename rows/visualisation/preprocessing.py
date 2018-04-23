c22e2de3fad1ff4d20b9b736c7ec2f61490d18b2
annalisa <nina1983@gmail.com>
2018-04-23 19:29:13 +0100

    fixing bugs

--- rows/visualisation/preprocessing.py
+++ rows/visualisation/preprocessing.py
@@ -1,7 +1,6 @@
 import dateutil.parser as parser
 from datetime import datetime, timedelta
 import xml.etree.ElementTree as ET
-import matplotlib.pyplot as plt
 import numpy as np
 
 #add namespace
@@ -19,56 +18,67 @@ tag = '{http://www.gexf.net/1.1draft}'
 sh= tree.find(tag+'graph') 
 sh.set('mode','dynamic')
 
+dict_att={}
+#create dictionary of attributes - IDs
+for atts in sh.findall(tag+'attributes'):
+    for att in atts.findall(tag+'attribute'):
+        dict_att[att.attrib['title']]=att.attrib['id']
+type_id = dict_att['type']
+sap_number_id = dict_att['sap_number']
+id_id = dict_att['id']
+assigned_carer_id = dict_att['assigned_carer']
+st = dict_att['start_time']
+dropped = dict_att['dropped']
+duration = dict_att['duration']
+work_relative = dict_att['work_relative']
+
 #add timeformat 
 timeformat = ET.Element(tag+'graph')
 for timeformat in root:
     timeformat.set('timeformat', 'datetime')
     
-#set start attribute to nodes that are visits
+#retrieve date of scheduling
 Value=""
 for eleme in sh.findall(tag+'nodes'):
     for ele in eleme.findall(tag+'node'):
         for el in ele.findall(tag+'attvalues'):
             for elm in el.findall(tag+'attvalue'):
                 start_attrib = elm.attrib
-                if start_attrib['for'] == "8":
+                if start_attrib['for'] == st:
                     Value = elm.get('value')
         if Value: 
             date1 = parser.parse(Value)
             ele.set('start', date1.isoformat())   
             Value=""
-
-#set default
-day = date1.isoformat()[0:7]    
+#set default time
+day = date1.isoformat()[0:10]    
 date_default = day+"T00:00:00"
 for elem in root.getiterator():
     if elem.text:
         elem.text= elem.text.replace('2000-Jan-01 00:00:00',date_default)
 
-#retrieve information about carers and visits
-dict_carer={'dropped':[]}
+#retrieve information about carers and visits (which carer service which visit and when the carer is ending the visits serviced)
+dict_carer={}
 dict_carer_endtime={}
 for nodes in sh.findall(tag+'nodes'):
     for node in nodes.findall(tag+'node'):
-        if 'carer' in node.attrib['label']:
-            for attvalues in node.findall(tag+'attvalues'):
-                for elm in attvalues.findall(tag+'attvalue'):
-                    if(elm.attrib['for']  == '11'):
-                        SAP_number = elm.attrib['value'] 
-                        dict_carer[SAP_number]=[]   
-                        dict_carer_endtime[SAP_number]=day+'T00:00:00'            
-        elif('visit' in node.attrib['label']):
-            for attvalues in node.findall(tag+'attvalues'):
-                for elm in attvalues.findall(tag+'attvalue'):
-                    if(elm.attrib['for']  == '4'):
-                        SAP_number = elm.attrib['value']
-                    if(elm.attrib['for']  == '5'):
-                        SAP_number = 'dropped'
-                    if(elm.attrib['for']  == '8'):
-                        t0 = elm.attrib['value']
-                    if(elm.attrib['for']  == '9'):
-                        dt = elm.attrib['value']
-            if SAP_number != 'dropped':               
+        for attvalues in node.findall(tag+'attvalues'):
+            attributes = {}            
+            for att in attvalues.findall(tag+'attvalue'):
+                attributes[att.attrib['for']] = att.attrib['value'] 
+        if attributes[type_id] == 'carer':
+            SAP_number = attributes[sap_number_id] 
+            dict_carer[SAP_number]=[]   
+            dict_carer_endtime[SAP_number]=day+'T00:00:00'            
+        elif attributes[type_id] == 'visit' :
+            if assigned_carer_id in attributes.keys():              
+                SAP_number = attributes[assigned_carer_id]
+                #append visit id to dictionary
+                id_visit = node.attrib['id']                        
+                dict_carer[SAP_number].append(id_visit)
+                #compute endtime visit and update endtime carer
+                t0 = attributes[st]
+                dt = attributes[duration]               
                 FMT = '%H:%M:%S'
                 parsed_time = datetime.strptime(t0[-8:], FMT)
                 parsed_duration = datetime.strptime(dt, FMT)
@@ -79,53 +89,53 @@ for nodes in sh.findall(tag+'nodes'):
                 tdelta = datetime.strptime(dict_carer_endtime[SAP_number][-8:], FMT) - datetime.strptime(result, FMT)
                 if  tdelta.total_seconds() < 0:           
                     dict_carer_endtime[SAP_number] = day+'T'+result
-            id_visit = node.attrib['label']                        
-            dict_carer[SAP_number].append(id_visit)
-                  
-for i in dict_carer.keys():
-    visits = dict_carer[i]
+            
+#setting up size and colors and endtime in the graph
 
-##setting up size and colors and endtime in the graph
 #create palette of colors
-
 n_carers = len(dict_carer.keys())
 colors_dict = {}
 for i in dict_carer.keys():
     colors_dict[i] = list(np.random.choice(range(256), size=3)) 
+colors_dict['dropped'] = [0, 0, 0] #setting color of dropped visits to black
 
 #set size and colors
 for nodes in sh.findall(tag+'nodes'):
     for node in nodes.findall(tag+'node'):
-        child_size = ET.SubElement(node, ET.QName(nsm['viz'], "size"), value='0.0')
-        child_col = ET.SubElement(node, ET.QName(nsm['viz'], "color"), value='0.0')
-        if 'carer' in node.attrib['label']:
+        for attvalues in node.findall(tag+'attvalues'):
+            attributes = {}            
+            for att in attvalues.findall(tag+'attvalue'):
+                attributes[att.attrib['for']] = att.attrib['value']  
+        if attributes[type_id] == 'carer':
+            child_size = ET.SubElement(node, ET.QName(nsm['viz'], "size"), value='0.0')
+            child_col = ET.SubElement(node, ET.QName(nsm['viz'], "color"))
             # setting start and end time for carers
             node.set('start', date_default)
-            for attvalues in node.findall(tag+'attvalues'):
-                for elm in attvalues.findall(tag+'attvalue'):
-                    if(elm.attrib['for']  == '12'):
-                        size_c = elm.attrib['value']
-                        child_size.set('value', size_c)
-                    if(elm.attrib['for']  == '11'):
-                        SAP_number = elm.attrib['value'] 
-                        color = colors_dict[SAP_number] 
-                        child_col.set('r', str(color[0]))                          
-                        child_col.set('g', str(color[1]))
-                        child_col.set('b', str(color[2]))
+            size_c = attributes[work_relative] 
+            child_size.set('value', size_c)
+            SAP_number = attributes[sap_number_id] 
+            color = colors_dict[SAP_number] 
+            child_col.set('r', str(color[0]))                          
+            child_col.set('g', str(color[1]))
+            child_col.set('b', str(color[2]))
             node.set('end', dict_carer_endtime[SAP_number])
-        elif('visit' in node.attrib['label']):
-            for attvalues in node.findall(tag+'attvalues'):
-                size_c = '0.5' 
-                child_size.set('value', size_c)        
-                for elm in attvalues.findall(tag+'attvalue'):
-                        if(elm.attrib['for']  == '4'):
-                            SAP_number = elm.attrib['value'] 
-                            color = colors_dict[SAP_number] 
-                            child_col.set('r', str(color[0]))                          
-                            child_col.set('g', str(color[1]))
-                            child_col.set('b', str(color[2]))
-            if SAP_number != 'dropped':
+        elif attributes[type_id]=='visit':
+            child_size = ET.SubElement(node, ET.QName(nsm['viz'], "size"), value='0.0')
+            child_col = ET.SubElement(node, ET.QName(nsm['viz'], "color"))
+            size_c = '0.5' 
+            child_size.set('value', size_c)        
+            if assigned_carer_id in attributes.keys():    
+                SAP_number = attributes[assigned_carer_id] 
+                color = colors_dict[SAP_number] 
+                child_col.set('r', str(color[0]))                          
+                child_col.set('g', str(color[1]))
+                child_col.set('b', str(color[2]))
                 node.set('end', dict_carer_endtime[SAP_number])
+            else:
+                color = colors_dict['dropped'] 
+                child_col.set('r', str(color[0]))                          
+                child_col.set('g', str(color[1]))
+                child_col.set('b', str(color[2]))
 
 #write final xml to file
 tree.write('solution_modified.gexf')           
