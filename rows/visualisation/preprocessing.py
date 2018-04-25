0515875732794be87bdaec106e12d36cd6e2bdf9
Annalisa Riccardi <nina1983@gmail.com>
2018-04-25 18:23:02 +0100

    removing edges between carers and visits and changing geolayout location of carers

--- rows/visualisation/preprocessing.py
+++ rows/visualisation/preprocessing.py
@@ -2,6 +2,7 @@ import dateutil.parser as parser
 from datetime import datetime, timedelta
 import xml.etree.ElementTree as ET
 import numpy as np
+import random
 
 #add namespace
 #registering namespaces
@@ -30,6 +31,8 @@ st = dict_att['start_time']
 dropped = dict_att['dropped']
 duration = dict_att['duration']
 work_relative = dict_att['work_relative']
+lon = dict_att['longitude']
+lat = dict_att['latitude']
 
 #add timeformat 
 timeformat = ET.Element(tag+'graph')
@@ -58,7 +61,11 @@ for elem in root.getiterator():
 
 #retrieve information about carers and visits (which carer service which visit and when the carer is ending the visits serviced)
 dict_carer={}
+dict_carer_id={}
 dict_carer_endtime={}
+dict_carer_starttime={}
+dict_carer_firstvisit_pos={}
+dict_carer_firstvisit_id={}
 for nodes in sh.findall(tag+'nodes'):
     for node in nodes.findall(tag+'node'):
         for attvalues in node.findall(tag+'attvalues'):
@@ -67,8 +74,12 @@ for nodes in sh.findall(tag+'nodes'):
                 attributes[att.attrib['for']] = att.attrib['value'] 
         if attributes[type_id] == 'carer':
             SAP_number = attributes[sap_number_id] 
-            dict_carer[SAP_number]=[]   
-            dict_carer_endtime[SAP_number]=day+'T00:00:00'            
+            dict_carer[SAP_number]=[]  
+            dict_carer_id[node.attrib['id']]=SAP_number   
+            dict_carer_endtime[SAP_number]=day+'T00:00:00'  
+            dict_carer_starttime[SAP_number]=day+'T23:59:59'
+            dict_carer_firstvisit_pos[SAP_number] = [0.0,0.0]          
+            dict_carer_firstvisit_id[SAP_number] = ""
         elif attributes[type_id] == 'visit' :
             if assigned_carer_id in attributes.keys():              
                 SAP_number = attributes[assigned_carer_id]
@@ -85,10 +96,16 @@ for nodes in sh.findall(tag+'nodes'):
                                minutes=parsed_duration.minute,
                                seconds=parsed_duration.second)
                 result = then.strftime(FMT)
-                tdelta = datetime.strptime(dict_carer_endtime[SAP_number][-8:], FMT) - datetime.strptime(result, FMT)
-                if  tdelta.total_seconds() < 0:           
+                tdelta_end = datetime.strptime(dict_carer_endtime[SAP_number][-8:], FMT) - datetime.strptime(result, FMT)
+                tdelta_start = datetime.strptime(parsed_time.strftime(FMT), FMT) - datetime.strptime(dict_carer_starttime[SAP_number][-8:], FMT)
+                if  tdelta_end.total_seconds() < 0:           
                     dict_carer_endtime[SAP_number] = day+'T'+result
-            
+                if  tdelta_start.total_seconds() < 0: 
+                    dict_carer_starttime[SAP_number] = day+'T'+parsed_time.strftime(FMT)
+                    dict_carer_firstvisit_pos[SAP_number][0] = float(attributes[lon])
+                    dict_carer_firstvisit_pos[SAP_number][1] = float(attributes[lat])
+                    dict_carer_firstvisit_id[SAP_number] = id_visit        
+
 #setting up size and colors and endtime in the graph
 
 #create palette of colors
@@ -109,15 +126,21 @@ for nodes in sh.findall(tag+'nodes'):
             child_size = ET.SubElement(node, ET.QName(nsm['viz'], "size"), value='0.0')
             child_col = ET.SubElement(node, ET.QName(nsm['viz'], "color"))
             # setting start and end time for carers
-            node.set('start', date_default)
             size_c = attributes[work_relative] 
-            child_size.set('value', size_c)
+            child_size.set('value', str(float(size_c)+1))
             SAP_number = attributes[sap_number_id] 
             color = colors_dict[SAP_number] 
             child_col.set('r', str(color[0]))                          
             child_col.set('g', str(color[1]))
             child_col.set('b', str(color[2]))
             node.set('end', dict_carer_endtime[SAP_number])
+            node.set('start', dict_carer_starttime[SAP_number])
+            lon_el = ET.SubElement(attvalues, ET.QName(nsm[""],"attvalue"))
+            lat_el = ET.SubElement(attvalues, ET.QName(nsm[""],"attvalue"))
+	    lon_el.set('for' , lon)
+            lat_el.set('for' , lat)
+	    lon_el.set('value' ,str(dict_carer_firstvisit_pos[SAP_number][0] -0.001+0.002*random.uniform(0, 1)) )
+            lat_el.set('value' ,str(dict_carer_firstvisit_pos[SAP_number][1] -0.001+0.002*random.uniform(0, 1)) )
         elif attributes[type_id]=='visit':
             child_size = ET.SubElement(node, ET.QName(nsm['viz'], "size"), value='0.0')
             child_col = ET.SubElement(node, ET.QName(nsm['viz'], "color"))
@@ -138,10 +161,18 @@ for nodes in sh.findall(tag+'nodes'):
         elif attributes[type_id]=='user':
             nodes.remove(node)
 
+print dict_carer_firstvisit_id
+print dict_carer_id
+
 for edges in sh.findall(tag+'edges'):
     for edge in edges.findall(tag+'edge'):
         if 'u' in edge.attrib['source']:
             edges.remove(edge)
+        if 'c' in edge.attrib['source']:
+            carer_id = edge.attrib['source']
+            carer_SAP = dict_carer_id[carer_id]
+            if edge.attrib['target'] != dict_carer_firstvisit_id[carer_SAP]:
+                edges.remove(edge)
 
 #write final xml to file
 tree.write('solution_modified.gexf')           
