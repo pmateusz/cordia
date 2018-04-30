64025a3403dd23f420dbee2d08a0576369a82a31
Nikolina-Charikleia Kotzamanidi <rjb16205@ds.strath.ac.uk>
2018-04-30 10:47:46 +0100

    graphs update

--- rows/visualisation/graphs.py
+++ rows/visualisation/graphs.py
@@ -14,6 +14,11 @@ carers=[]
 serv_t=[]
 trav_t=[]
 idle_t=[]
+stime=[]
+dropped=[]
+count=0
+l=[]
+flag = False
 for eleme in sh.findall('{http://www.gexf.net/1.1draft}nodes'):
     for ele in eleme.findall('{http://www.gexf.net/1.1draft}node'):
          if 'carer' in ele.attrib['label']:
@@ -43,6 +48,23 @@ for eleme in sh.findall('{http://www.gexf.net/1.1draft}nodes'):
                          tme2 = dt2.time()
                          delta2 = timedelta(hours=tme2.hour, minutes=tme2.minute, seconds=tme2.second)
                          idle_t.append(delta2)
+         if 'visit' in ele.attrib['label']:
+           for el in ele.findall('{http://www.gexf.net/1.1draft}attvalues'):
+              for elm in el.findall('{http://www.gexf.net/1.1draft}attvalue'):
+                 start_attrib = elm.attrib
+                 if start_attrib['for'] == "8":
+                     Value = elm.get('value')
+                     Val = Value[-8:]
+                     stime.append(Val)
+                 if start_attrib['for'] == "5":
+                    flag = True
+                    l.append(count)
+                    Dropped = elm.get('value')
+                    dropped.append(Dropped)
+           if flag != True:
+               dropped.append(False) 
+           else:
+               flag = False
 
 carers_dict= dict(zip(carers, zip(serv_t, trav_t, idle_t)))
 df = pd.DataFrame.from_dict(carers_dict, orient='index')
@@ -68,6 +90,42 @@ for col_num in range(1, len(col) + 1):
 chart.set_y_axis({'major_gridlines': {'visible': False}})
 worksheet.insert_chart('K2', chart)
 
+df1= pd.DataFrame(list(zip(stime, dropped)))
+df1.columns = ['stime', 'dropped']
+visits=df1.stime.str[:2].value_counts()
+total_visits = dict(visits)
+    
+hours = df1.stime.str[:2]
+drop = df1.dropped
+counter = {}
+
+for i in range(0,len(hours)):
+    if hours[i] in counter.keys():
+        if drop[i]:
+            counter[hours[i]] = counter[hours[i]]+1
+    else:
+        counter[hours[i]] = 0
+
+visits_dict = {key:(total_visits[key], counter[key]) for key in total_visits}
+df2= pd.DataFrame.from_dict(visits_dict, orient='index')
+df2.columns = ['total visits', 'dropped visits']
+col2 = ['total visits', 'dropped visits']
+index=df2.index.values.tolist()
+df2.index.name = 'hour'
+df2.sort_index(inplace=True)
+df2.to_excel(writer, sheet_name='Sheet2')
+workbook  = writer.book
+worksheet = writer.sheets['Sheet2']
+chart1 = workbook.add_chart({'type': 'column'})
+for coln in range(1, len(col2) + 1):
+     chart1.add_series({
+        'name':       ['Sheet2', 0, coln],
+        'categories': ['Sheet2', 1, 0, len(index), 0],
+        'values':     ['Sheet2', 1, coln, len(index), coln],
+        'gap':        300,
+    })
+chart1.set_y_axis({'major_gridlines': {'visible': False}})
+worksheet.insert_chart('K2', chart1)
 
 writer.save()
 
