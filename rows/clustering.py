2701ebf232d30ed96789b027262f206052fbb05d
Mateusz Polnik <mateusz.polnik@strath.ac.uk>
2018-05-11 09:54:21 +0100

    Prepare for time series analysis

--- rows/clustering.py
+++ rows/clustering.py
@@ -5,8 +5,8 @@ import collections
 import datetime
 import functools
 import itertools
-import logging
 import os
+import sklearn
 
 import numpy
 
@@ -14,74 +14,14 @@ import analysis
 
 import numpy as np
 
-from sklearn.cluster import DBSCAN, SpectralClustering, MeanShift, MiniBatchKMeans, AffinityPropagation, \
-    AgglomerativeClustering
-from sklearn import metrics
-from sklearn.datasets.samples_generator import make_blobs
-from sklearn.preprocessing import StandardScaler
-
 import matplotlib.pyplot
 import matplotlib.pylab
 
-import tqdm
 
-
-def example():
-    # #############################################################################
-    # Generate sample data
-    centers = [[1, 1], [-1, -1], [1, -1]]
-    X, labels_true = make_blobs(n_samples=750, centers=centers, cluster_std=0.4,
-                                random_state=0)
-
-    X = StandardScaler().fit_transform(X)
-
-    # #############################################################################
-    # Compute DBSCAN
-    db = DBSCAN(eps=0.3, min_samples=10).fit(X)
-    core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
-    core_samples_mask[db.core_sample_indices_] = True
-    labels = db.labels_
-
-    # Number of clusters in labels, ignoring noise if present.
-    n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
-
-    print('Estimated number of clusters: %d' % n_clusters_)
-    print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels_true, labels))
-    print("Completeness: %0.3f" % metrics.completeness_score(labels_true, labels))
-    print("V-measure: %0.3f" % metrics.v_measure_score(labels_true, labels))
-    print("Adjusted Rand Index: %0.3f"
-          % metrics.adjusted_rand_score(labels_true, labels))
-    print("Adjusted Mutual Information: %0.3f"
-          % metrics.adjusted_mutual_info_score(labels_true, labels))
-    print("Silhouette Coefficient: %0.3f"
-          % metrics.silhouette_score(X, labels))
-
-    # #############################################################################
-    # Plot result
-    import matplotlib.pyplot as plt
-
-    # Black removed and is used for noise instead.
-    unique_labels = set(labels)
-    colors = [plt.cm.Spectral(each)
-              for each in np.linspace(0, 1, len(unique_labels))]
-    for k, col in zip(unique_labels, colors):
-        if k == -1:
-            # Black used for noise.
-            col = [0, 0, 0, 1]
-
-        class_member_mask = (labels == k)
-
-        xy = X[class_member_mask & core_samples_mask]
-        plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
-                 markeredgecolor='k', markersize=14)
-
-        xy = X[class_member_mask & ~core_samples_mask]
-        plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
-                 markeredgecolor='k', markersize=6)
-
-    plt.title('Estimated number of clusters: %d' % n_clusters_)
-    plt.show()
-
+# select a user
+# develop a model with prediction
+# plot the model and errors
+# compare with errors that real humans do
 
 def load_visits(file_path):
     with open(file_path, 'r') as stream_reader:
@@ -235,6 +175,84 @@ def clear_time(value):
                              0)
 
 
+def plot_clusters(clusters, output_dir):
+    group_handles = []
+    fig, ax = matplotlib.pyplot.subplots()
+    __COLOR_MAP = 'tab10'
+    matplotlib.pyplot.set_cmap(__COLOR_MAP)
+    color_map = matplotlib.cm.get_cmap(__COLOR_MAP)
+    color_it = iter(color_map.colors)
+    shapes = ['s', 'o', 'D', 'X']
+    shape_it = iter(shapes)
+    task_shapes = {}
+    label_colors = {}
+
+    for cluster in clusters:
+        task_groups = collections.defaultdict(list)
+        for visit in cluster.items:
+            task_groups[visit.tasks].append(visit)
+
+        for tasks, group in task_groups.items():
+            fill_color = 'w'
+            edge_color = 'black'
+            shape = 's'
+            edge_width = 0.5
+            size = 8
+
+            if cluster.label != -1:
+                edge_width = 0
+                item = group[0]
+                if item.tasks in task_shapes:
+                    shape = task_shapes[item.tasks]
+                else:
+                    shape = next(shape_it, '.')
+                    task_shapes[item.tasks] = shape
+
+                if cluster.label in label_colors:
+                    fill_color = label_colors[cluster.label]
+                else:
+                    fill_color = next(color_it, None)
+                    label_colors[cluster.label] = fill_color
+
+            start_days = []
+            start_times = []
+            for visit in group:
+                start_days.append(clear_time(visit.original_start))
+                start_times.append(clear_date(visit.original_start))
+            task_label_handle = ax.scatter(start_days,
+                                           start_times,
+                                           c=fill_color,
+                                           marker=shape,
+                                           linewidth=edge_width,
+                                           edgecolor=edge_color,
+                                           alpha=0.7,
+                                           s=size)
+
+            group_handles.append((task_label_handle, get_series_name(group, cluster)))
+
+    ax.yaxis.set_major_formatter(matplotlib.dates.DateFormatter('%H:%M'))
+    ax.set_ylim(__ZERO_DATE + datetime.timedelta(hours=6), __ZERO_DATE + datetime.timedelta(hours=23, minutes=50))
+    matplotlib.pyplot.xticks(rotation=-60)
+
+    chart_box = ax.get_position()
+    ax.set_position([chart_box.x0, chart_box.y0, chart_box.width, chart_box.height * 0.75])
+    ax.grid(True)
+
+    group_handles.sort(key=lambda item: item[1])
+
+    legend = matplotlib.pylab.legend(map(lambda item: item[0], group_handles),
+                                     map(lambda item: item[1], group_handles),
+                                     loc=9,
+                                     bbox_to_anchor=(0.5, -0.30),
+                                     ncol=2,
+                                     shadow=False,
+                                     frameon=True)
+    matplotlib.pyplot.savefig(os.path.join(output_dir, str(user_id) + '.png'),
+                              additional_artists=(legend,),
+                              bbox_inches='tight')
+    matplotlib.pyplot.close(fig)
+
+
 if __name__ == '__main__':
     visits = load_visits('/home/pmateusz/dev/cordia/output.csv')
     output_dir = '/home/pmateusz/dev/cordia/data/clustering'
@@ -259,8 +277,9 @@ if __name__ == '__main__':
         distances = calculate_distance(raw_visits_to_use, distance)
         eps_threshold = -1
         cluster_count = -1
+        clusters = []
         for eps_threshold, min_samples in zip([91, 76, 61, 46, 31, 17], [8, 8, 8, 16, 16, 16]):
-            db = DBSCAN(eps=eps_threshold, min_samples=min_samples, metric='precomputed')
+            db = sklearn.cluster.DBSCAN(eps=eps_threshold, min_samples=min_samples, metric='precomputed')
             db.fit(distances)
 
             labels = db.labels_
@@ -280,78 +299,4 @@ if __name__ == '__main__':
                 break
 
         print('User=({0}) Clusters=({1}) Threshold=({2})'.format(user_id, cluster_count, eps_threshold))
-        group_handles = []
-        fig, ax = matplotlib.pyplot.subplots()
-        __COLOR_MAP = 'tab10'
-        matplotlib.pyplot.set_cmap(__COLOR_MAP)
-        color_map = matplotlib.cm.get_cmap(__COLOR_MAP)
-        color_it = iter(color_map.colors)
-        shapes = ['s', 'o', 'D', 'X']
-        shape_it = iter(shapes)
-        task_shapes = {}
-        label_colors = {}
-
-        for cluster in clusters:
-            task_groups = collections.defaultdict(list)
-            for visit in cluster.items:
-                task_groups[visit.tasks].append(visit)
-
-            for tasks, group in task_groups.items():
-                fill_color = 'w'
-                edge_color = 'black'
-                shape = 's'
-                edge_width = 0.5
-                size = 8
-
-                if cluster.label != -1:
-                    edge_width = 0
-                    item = group[0]
-                    if item.tasks in task_shapes:
-                        shape = task_shapes[item.tasks]
-                    else:
-                        shape = next(shape_it, '.')
-                        task_shapes[item.tasks] = shape
-
-                    if cluster.label in label_colors:
-                        fill_color = label_colors[cluster.label]
-                    else:
-                        fill_color = next(color_it, None)
-                        label_colors[cluster.label] = fill_color
-
-                start_days = []
-                start_times = []
-                for visit in group:
-                    start_days.append(clear_time(visit.original_start))
-                    start_times.append(clear_date(visit.original_start))
-                task_label_handle = ax.scatter(start_days,
-                                               start_times,
-                                               c=fill_color,
-                                               marker='s',
-                                               linewidth=edge_width,
-                                               edgecolor=edge_color,
-                                               alpha=0.7,
-                                               s=size)
-
-                group_handles.append((task_label_handle, get_series_name(group, cluster)))
-
-        ax.yaxis.set_major_formatter(matplotlib.dates.DateFormatter('%H:%M'))
-        ax.set_ylim(__ZERO_DATE + datetime.timedelta(hours=6), __ZERO_DATE + datetime.timedelta(hours=23, minutes=50))
-        matplotlib.pyplot.xticks(rotation=-60)
-
-        chart_box = ax.get_position()
-        ax.set_position([chart_box.x0, chart_box.y0, chart_box.width, chart_box.height * 0.75])
-        ax.grid(True)
-
-        group_handles.sort(key=lambda item: item[1])
-
-        legend = matplotlib.pylab.legend(map(lambda item: item[0], group_handles),
-                                         map(lambda item: item[1], group_handles),
-                                         loc=9,
-                                         bbox_to_anchor=(0.5, -0.30),
-                                         ncol=2,
-                                         shadow=False,
-                                         frameon=True)
-        matplotlib.pyplot.savefig(os.path.join(output_dir, str(user_id) + '.png'),
-                                  additional_artists=(legend,),
-                                  bbox_inches='tight')
-        matplotlib.pyplot.close(fig)
+        plot_clusters(clusters, output_dir)
