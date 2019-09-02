73aa89a91ec72f54524f094b9a5f0ce7f16be6f5
pmateusz <mat.polnik@gmail.com>
2019-09-02 09:39:02 +0100

    Excel spreadsheet

---
+++ generate_pilot_excel.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# ./rows_cli.py visualise ./september-pilot/c380_october_problem.json ./september-pilot/c380redv30b30e15_20181001.gexf --output ./september-pilot/c380redv30b30e15_20181001
+# ./rows_cli.py visualise ./september-pilot/c380_october_problem.json ./september-pilot/c380redv30b30e15_20181002.gexf --output ./september-pilot/c380redv30b30e15_20181002
+# ./rows_cli.py visualise ./september-pilot/c380_october_problem.json ./september-pilot/c380redv30b30e15_20181003.gexf --output ./september-pilot/c380redv30b30e15_20181003
+# ./rows_cli.py visualise ./september-pilot/c380_october_problem.json ./september-pilot/c380redv30b30e15_20181004.gexf --output ./september-pilot/c380redv30b30e15_20181004
+# ./rows_cli.py visualise ./september-pilot/c380_october_problem.json ./september-pilot/c380redv30b30e15_20181005.gexf --output ./september-pilot/c380redv30b30e15_20181005
+# ./rows_cli.py visualise ./september-pilot/c380_october_problem.json ./september-pilot/c380redv30b30e15_20181006.gexf --output ./september-pilot/c380redv30b30e15_20181006
+# ./rows_cli.py visualise ./september-pilot/c380_october_problem.json ./september-pilot/c380redv30b30e15_20181007.gexf --output ./september-pilot/c380redv30b30e15_20181007
+# ./rows_cli.py visualise ./september-pilot/c380_october_problem.json ./september-pilot/c380redv30b30e15_20181008.gexf --output ./september-pilot/c380redv30b30e15_20181008
+# ./rows_cli.py visualise ./september-pilot/c380_october_problem.json ./september-pilot/c380redv30b30e15_20181009.gexf --output ./september-pilot/c380redv30b30e15_20181009
+# ./rows_cli.py visualise ./september-pilot/c380_october_problem.json ./september-pilot/c380redv30b30e15_20181010.gexf --output ./september-pilot/c380redv30b30e15_20181010
+# ./rows_cli.py visualise ./september-pilot/c380_october_problem.json ./september-pilot/c380redv30b30e15_20181011.gexf --output ./september-pilot/c380redv30b30e15_20181011
+# ./rows_cli.py visualise ./september-pilot/c380_october_problem.json ./september-pilot/c380redv30b30e15_20181012.gexf --output ./september-pilot/c380redv30b30e15_20181012
+# ./rows_cli.py visualise ./september-pilot/c380_october_problem.json ./september-pilot/c380redv30b30e15_20181013.gexf --output ./september-pilot/c380redv30b30e15_20181013
+
+# ./rows_cli.py visualise ./september-pilot/c270_october_problem.json ./september-pilot/c270redv30b30e15_20181001.gexf --output ./september-pilot/c270redv30b30e15_20181001
+# ./rows_cli.py visualise ./september-pilot/c270_october_problem.json ./september-pilot/c270redv30b30e15_20181002.gexf --output ./september-pilot/c270redv30b30e15_20181002
+# ./rows_cli.py visualise ./september-pilot/c270_october_problem.json ./september-pilot/c270redv30b30e15_20181003.gexf --output ./september-pilot/c270redv30b30e15_20181003
+# ./rows_cli.py visualise ./september-pilot/c270_october_problem.json ./september-pilot/c270redv30b30e15_20181004.gexf --output ./september-pilot/c270redv30b30e15_20181004
+# ./rows_cli.py visualise ./september-pilot/c270_october_problem.json ./september-pilot/c270redv30b30e15_20181005.gexf --output ./september-pilot/c270redv30b30e15_20181005
+# ./rows_cli.py visualise ./september-pilot/c270_october_problem.json ./september-pilot/c270redv30b30e15_20181006.gexf --output ./september-pilot/c270redv30b30e15_20181006
+# ./rows_cli.py visualise ./september-pilot/c270_october_problem.json ./september-pilot/c270redv30b30e15_20181007.gexf --output ./september-pilot/c270redv30b30e15_20181007
+# ./rows_cli.py visualise ./september-pilot/c270_october_problem.json ./september-pilot/c270redv30b30e15_20181008.gexf --output ./september-pilot/c270redv30b30e15_20181008
+# ./rows_cli.py visualise ./september-pilot/c270_october_problem.json ./september-pilot/c270redv30b30e15_20181009.gexf --output ./september-pilot/c270redv30b30e15_20181009
+# ./rows_cli.py visualise ./september-pilot/c270_october_problem.json ./september-pilot/c270redv30b30e15_20181010.gexf --output ./september-pilot/c270redv30b30e15_20181010
+# ./rows_cli.py visualise ./september-pilot/c270_october_problem.json ./september-pilot/c270redv30b30e15_20181011.gexf --output ./september-pilot/c270redv30b30e15_20181011
+# ./rows_cli.py visualise ./september-pilot/c270_october_problem.json ./september-pilot/c270redv30b30e15_20181012.gexf --output ./september-pilot/c270redv30b30e15_20181012
+# ./rows_cli.py visualise ./september-pilot/c270_october_problem.json ./september-pilot/c270redv30b30e15_20181013.gexf --output ./september-pilot/c270redv30b30e15_20181013
+
+./rows_cli.py visualise ./september-pilot/c100_october_problem_compare.json ./september-pilot/c100redv60b60e15_20181002.gexf --output ./september-pilot/c100redv60b60e15_20181002
+./rows_cli.py visualise ./september-pilot/c100_october_problem_compare.json ./september-pilot/c100redv60b60e15_20181003.gexf --output ./september-pilot/c100redv60b60e15_20181003
+./rows_cli.py visualise ./september-pilot/c100_october_problem_compare.json ./september-pilot/c100redv60b60e15_20181004.gexf --output ./september-pilot/c100redv60b60e15_20181004
+
+./rows_cli.py visualise ./september-pilot/c270_october_problem_compare.json ./september-pilot/c270redv60b60e15_20181002.gexf --output ./september-pilot/c270redv60b60e15_20181002
+./rows_cli.py visualise ./september-pilot/c270_october_problem_compare.json ./september-pilot/c270redv60b60e15_20181003.gexf --output ./september-pilot/c270redv60b60e15_20181003
+./rows_cli.py visualise ./september-pilot/c270_october_problem_compare.json ./september-pilot/c270redv60b60e15_20181004.gexf --output ./september-pilot/c270redv60b60e15_20181004
+
+./rows_cli.py visualise ./september-pilot/c380_october_problem_compare.json ./september-pilot/c380redv60b60e15_20181002.gexf --output ./september-pilot/c380redv60b60e15_20181002
+./rows_cli.py visualise ./september-pilot/c380_october_problem_compare.json ./september-pilot/c380redv60b60e15_20181003.gexf --output ./september-pilot/c380redv60b60e15_20181003
+./rows_cli.py visualise ./september-pilot/c380_october_problem_compare.json ./september-pilot/c380redv60b60e15_20181004.gexf --output ./september-pilot/c380redv60b60e15_20181004
+
+#./rows_cli.py visualise ./september-pilot/c100_october_problem.json ./september-pilot/c100redv30b30e15_20181001.gexf --output ./september-pilot/c100redv30b30e15_20181001
+#./rows_cli.py visualise ./september-pilot/c100_october_problem.json ./september-pilot/c100redv30b30e15_20181002.gexf --output ./september-pilot/c100redv30b30e15_20181002
+#./rows_cli.py visualise ./september-pilot/c100_october_problem.json ./september-pilot/c100redv30b30e15_20181003.gexf --output ./september-pilot/c100redv30b30e15_20181003
+#./rows_cli.py visualise ./september-pilot/c100_october_problem.json ./september-pilot/c100redv30b30e15_20181004.gexf --output ./september-pilot/c100redv30b30e15_20181004
+#./rows_cli.py visualise ./september-pilot/c100_october_problem.json ./september-pilot/c100redv30b30e15_20181005.gexf --output ./september-pilot/c100redv30b30e15_20181005
+#./rows_cli.py visualise ./september-pilot/c100_october_problem.json ./september-pilot/c100redv30b30e15_20181006.gexf --output ./september-pilot/c100redv30b30e15_20181006
+#./rows_cli.py visualise ./september-pilot/c100_october_problem.json ./september-pilot/c100redv30b30e15_20181007.gexf --output ./september-pilot/c100redv30b30e15_20181007
+#./rows_cli.py visualise ./september-pilot/c100_october_problem.json ./september-pilot/c100redv30b30e15_20181008.gexf --output ./september-pilot/c100redv30b30e15_20181008
+#./rows_cli.py visualise ./september-pilot/c100_october_problem.json ./september-pilot/c100redv30b30e15_20181009.gexf --output ./september-pilot/c100redv30b30e15_20181009
+#./rows_cli.py visualise ./september-pilot/c100_october_problem.json ./september-pilot/c100redv30b30e15_20181010.gexf --output ./september-pilot/c100redv30b30e15_20181010
+#./rows_cli.py visualise ./september-pilot/c100_october_problem.json ./september-pilot/c100redv30b30e15_20181011.gexf --output ./september-pilot/c100redv30b30e15_20181011
+#./rows_cli.py visualise ./september-pilot/c100_october_problem.json ./september-pilot/c100redv30b30e15_20181012.gexf --output ./september-pilot/c100redv30b30e15_20181012
+#./rows_cli.py visualise ./september-pilot/c100_october_problem.json ./september-pilot/c100redv30b30e15_20181013.gexf --output ./september-pilot/c100redv30b30e15_20181013
